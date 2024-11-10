6da619152c9187523efbec61291a60331d10d3ef
Piyush arya <piyush.arya@letzcreate.in>
2024-11-11 03:24:42 +0530

    Xlogin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const[name,setName]=useState('');
+  const[pass,setPass]=useState('');
+  const [error, setError] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const validUsername = "user";
+  const validPassword = "password";
+  function login(e){
+    e.preventDefault();
+    if(!name || !pass){
+      setError("Please enter both username and password.")
+      return;
+    }
+    if (name === validUsername && pass === validPassword) {
+      setIsAuthenticated(true);
+      setError("");
+    } else {
+      setError("Invalid username or password");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {isAuthenticated ?(
+      <div>
+        <h1>Login Page</h1>
+        <h5>Welcome, user</h5>
+      </div>
+    ):(
+      <form onSubmit={login}>
+        <label>Username:</label>
+    <input type="text" value={name} placeholder="username" required onChange={(e)=>{
+        setName(e.target.value);
+      }}/>
+      
+      <br/>
+      <label>Password:</label>
+      <input type="password" placeholder="password" value={pass} required onChange={(e)=>{
+        setPass(e.target.value);
+      }}/>
+      
+      <br/>
+      {error && <p>{error}</p>}
+      <button type="submit">Submit</button>
+    </form>
+    )}
+    
+    
+    </>
   );
 }
 
